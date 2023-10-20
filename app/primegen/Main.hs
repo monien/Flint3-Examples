696e10d40b4c05fff2a78bef67998d0f5fea1e03
Hartmut Monien <3356162+monien@users.noreply.github.com>
2023-10-20 09:26:00 +0200

    updated primegen.

--- app/primegen/Main.hs
+++ app/primegen/Main.hs
@@ -1,30 +1,24 @@
 import Options.Applicative
 import Control.Monad
 import Foreign.C.Types
-import Data.Time
-import System.TimeIt
 
 import Data.Number.Flint
 
-main = timeItNamed "time" $ run =<< execParser opts where
+main = run =<< execParser opts where
   opts = info (parameters <**> helper) (
          fullDesc
       <> progDesc "Generate primes."
       <> header "Generate primes.")
 
-run params@(Parameters n nthreads) = do
+run params@(Parameters n count) = do
   withNewNPrimes $ \prime_iter -> do
     replicateM_ n $ do
       prime <- n_primes_next prime_iter
-      -- print prime
-      when (prime < 100) $ do
-        withFmpz x $ \x -> do
-          fmpz_mul_ui x x prime
-        return ()
+      print prime
   
 data Parameters = Parameters {
-    n :: CULong
-  , nthreads :: CInt
+    n :: Int
+  , count :: Bool
 } deriving Show
 
 parameters :: Parser Parameters
@@ -33,10 +27,12 @@ parameters = Parameters
     help "n" <>
     metavar "n")
   <*> option auto (
-    help "number of threads" <>
-    long "threads" <>
-    value 1 <>
-    metavar "threads")
+    help "count primes." <>
+    short 'c' <>
+    long "count" <>
+    value False <>
+    metavar "count")
+
 
   
   
